ab19f93f26e9c77847a4f04648e3b32a193b20ed
Ajay Kumar <ajaykan47@gmail.com>
2023-01-27 20:42:16 +0530

    CMS-5 - Setup basic page folders in the project

---
+++ src/pages/admin/dasboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const Dashboard = () => {
+  return (
+    <div>Dashboard</div>
+  );
+}
+
+export default Dashboard;
